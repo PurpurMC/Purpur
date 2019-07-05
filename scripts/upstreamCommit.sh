6d563f8de119ee3af64c00cf93eb0644c85b3c1e
William Blake Galbreath <blake.galbreath@gmail.com>
2019-07-04 21:30:16 -0500

    Updated Upstream (Paper)
    Upstream has released updates that appears to apply and compile correctly
    
    Paper Changes:
    c8bc57c7 Cleanup ChunkMapDistance patch
    276afaa2 Updated Upstream (Bukkit/CraftBukkit/Spigot)

---
+++ scripts/upstreamCommit.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+(
+set -e
+PS1="$"
+
+function changelog() {
+    base=$(git ls-tree HEAD $1  | cut -d' ' -f3 | cut -f1)
+    cd $1 && git log --oneline ${base}...HEAD
+}
+paper=$(changelog Paper)
+
+updated=""
+logsuffix=""
+if [ ! -z "$paper" ]; then
+    logsuffix="$logsuffix\n\nPaper Changes:\n$paper"
+    if [ -z "$updated" ]; then updated="Paper"; else updated="$updated/Paper"; fi
+fi
+disclaimer="Upstream has released updates that appears to apply and compile correctly"
+
+if [ ! -z "$1" ]; then
+    disclaimer="$@"
+fi
+
+log="${UP_LOG_PREFIX}Updated Upstream ($updated)\n\n${disclaimer}${logsuffix}"
+
+echo -e "$log" | git commit -F -
+
+) || exit 1
