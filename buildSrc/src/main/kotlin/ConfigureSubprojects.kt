9f4d68d047b364ff14a4c41636f4e630d42eca80
jmp <jasonpenilla2@me.com>
2020-12-15 22:55:12 -0800

    Fix Maven publication

--- buildSrc/src/main/kotlin/ConfigureSubprojects.kt
+++ buildSrc/src/main/kotlin/ConfigureSubprojects.kt
@@ -16,85 +16,93 @@ import org.gradle.kotlin.dsl.get
 import org.gradle.kotlin.dsl.getValue
 import org.gradle.kotlin.dsl.getting
 
-@Suppress("UNUSED_VARIABLE")
 internal fun Project.configureSubprojects() {
     subprojects {
-
         apply<JavaLibraryPlugin>()
         apply<MavenPublishPlugin>()
 
-        extensions.configure(PublishingExtension::class.java) {
-            publications {
-                create<MavenPublication>("mavenJava") {
-                    artifactId = if (project.name.endsWith("server")) rootProject.name else project.name
-                    groupId = rootProject.group as String
-                    version = rootProject.version as String
-                    from(components["java"])
-                    pom {
-                        name.set(project.name)
-                        url.set("https://github.com/pl3xgaming/Purpur")
+        project.afterEvaluate {
+            extensions.configure(PublishingExtension::class.java) {
+                publications {
+                    create<MavenPublication>("mavenJava") {
+                        artifactId = if (project.name.endsWith("server")) rootProject.name else project.name
+                        groupId = rootProject.group as String
+                        version = rootProject.version as String
+                        from(components["java"])
+                        pom {
+                            name.set(project.name)
+                            url.set("https://github.com/pl3xgaming/Purpur")
+                        }
                     }
                 }
+            }
+
+            when {
+                project.name.endsWith("server") -> configureServerProject()
+                project.name.endsWith("api") -> configureApiProject()
             }
         }
+    }
+}
 
-        if (project.name.endsWith("server")) {
-            apply<ShadowPlugin>()
+@Suppress("UNUSED_VARIABLE")
+private fun Project.configureServerProject() {
+    apply<ShadowPlugin>()
 
-            val generatePomFileForMavenJavaPublication by tasks.getting(GenerateMavenPom::class) {
-                destination = project.buildDir.resolve("tmp/pom.xml")
-            }
+    val generatePomFileForMavenJavaPublication by tasks.getting(GenerateMavenPom::class) {
+        destination = project.buildDir.resolve("tmp/pom.xml")
+    }
 
-            val test by tasks.getting(Test::class) {
-                // didn't bother to look into why these fail. paper excludes them in paperweight as well though
-                exclude("org/bukkit/craftbukkit/inventory/ItemStack*Test.class")
-            }
+    @Suppress("UNUSED_VARIABLE")
+    val test by tasks.getting(Test::class) {
+        // didn't bother to look into why these fail. paper excludes them in paperweight as well though
+        exclude("org/bukkit/craftbukkit/inventory/ItemStack*Test.class")
+    }
 
-            project.afterEvaluate {
-                val shadowJar by tasks.getting(ShadowJar::class) {
-                    dependsOn(generatePomFileForMavenJavaPublication)
-                    transform(Log4j2PluginsCacheFileTransformer::class.java)
-                    manifest {
-                        attributes(
-                            "Main-Class" to "org.bukkit.craftbukkit.Main",
-                            "Implementation-Title" to "CraftBukkit",
-                            "Implementation-Version" to toothpick.forkVersion,
-                            "Implementation-Vendor" to java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
-                                .format(java.util.Date()),
-                            "Specification-Title" to "Bukkit",
-                            "Specification-Version" to "${project.version}",
-                            "Specification-Vendor" to "Bukkit Team"
-                        )
-                    }
-                    from(project.buildDir.resolve("tmp/pom.xml")) {
-                        // dirty hack to make "java -Dpaperclip.install=true -jar paperclip.jar" work without forking paperclip
-                        into("META-INF/maven/io.papermc.paper/paper")
-                    }
+    val shadowJar by tasks.getting(ShadowJar::class) {
+        dependsOn(generatePomFileForMavenJavaPublication)
+        transform(Log4j2PluginsCacheFileTransformer::class.java)
+        manifest {
+            attributes(
+                "Main-Class" to "org.bukkit.craftbukkit.Main",
+                "Implementation-Title" to "CraftBukkit",
+                "Implementation-Version" to toothpick.forkVersion,
+                "Implementation-Vendor" to java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+                    .format(java.util.Date()),
+                "Specification-Title" to "Bukkit",
+                "Specification-Version" to "${project.version}",
+                "Specification-Vendor" to "Bukkit Team"
+            )
+        }
+        from(project.buildDir.resolve("tmp/pom.xml")) {
+            // dirty hack to make "java -Dpaperclip.install=true -jar paperclip.jar" work without forking paperclip
+            into("META-INF/maven/io.papermc.paper/paper")
+        }
 
-                    // Don't like to do this but sadly have to do this for compatibility reasons
-                    val relocVersion = toothpick.minecraftVersion.replace(".", "_")
-                    relocate("org.bukkit.craftbukkit", "org.bukkit.craftbukkit.v$relocVersion") {
-                        exclude("org.bukkit.craftbukkit.Main*")
-                    }
-                    relocate("net.minecraft.server", "net.minecraft.server.v$relocVersion")
-                }
-                tasks.getByName("build") {
-                    dependsOn(shadowJar)
-                }
-            }
-        } else if (project.name.endsWith("api")) {
-            val jar by this.tasks.getting(Jar::class) {
-                doFirst {
-                    buildDir.resolve("tmp/pom.properties")
-                        .writeText("version=${project.version}")
-                }
-                from(buildDir.resolve("tmp/pom.properties")) {
-                    into("META-INF/maven/${project.group}/${project.name}")
-                }
-                manifest {
-                    attributes("Automatic-Module-Name" to "org.bukkit")
-                }
-            }
+        // Don't like to do this but sadly have to do this for compatibility reasons
+        val relocVersion = toothpick.minecraftVersion.replace(".", "_")
+        relocate("org.bukkit.craftbukkit", "org.bukkit.craftbukkit.v$relocVersion") {
+            exclude("org.bukkit.craftbukkit.Main*")
+        }
+        relocate("net.minecraft.server", "net.minecraft.server.v$relocVersion")
+    }
+    tasks.getByName("build") {
+        dependsOn(shadowJar)
+    }
+}
+
+@Suppress("UNUSED_VARIABLE")
+private fun Project.configureApiProject() {
+    val jar by this.tasks.getting(Jar::class) {
+        doFirst {
+            buildDir.resolve("tmp/pom.properties")
+                .writeText("version=${project.version}")
+        }
+        from(buildDir.resolve("tmp/pom.properties")) {
+            into("META-INF/maven/${project.group}/${project.name}")
+        }
+        manifest {
+            attributes("Automatic-Module-Name" to "org.bukkit")
         }
     }
 }
