5e89d2377da79e6bae38977dc2b8101c68bc537d
jmp <jasonpenilla2@me.com>
2021-04-07 20:24:49 -0700

    Updated Upstream (Paper & Airplane)
    Upstream has released updates that appear to apply and compile correctly
    
    Paper Changes:
    3db3aaf39 [Auto] Updated Upstream (CraftBukkit)
    c953e51dd [Auto] Updated Upstream (CraftBukkit/Spigot)
    dc529c7a9 Fix PlayerEditBookEvent (#5463)
    3fea87edb [Auto] Updated Upstream (CraftBukkit)
    a111b1365 Send post ChatEvent messages as MessageType.CHAT
    d15161114 [Auto] Updated Upstream (Spigot)
    e8889e96a [Auto] Updated Upstream (CraftBukkit)
    3bc888ba6 [Auto] Updated Upstream (CraftBukkit)
    211f8e041 Prevent light queue overfill when no players are online
    
    Airplane Changes:
    3ed988c05 Use AIR library for configuration parsing

--- subprojects/server.gradle.kts
+++ subprojects/server.gradle.kts
@@ -1,3 +1,10 @@
+repositories {
+    // Airplane libs...
+    maven("https://jitpack.io") {
+        content { includeGroupByRegex("com\\.github\\..*") }
+    }
+}
+
 dependencies {
     implementation("cat.inspiracio", "rhino-js-engine", "1.7.7.1")
 }
