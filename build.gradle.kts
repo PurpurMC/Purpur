9beb23b8c388c3c00357f74f22d4d1d9044bfe50
Ben Kerllenevich <ben@omega24.dev>
2021-11-23 08:10:26 -0500

    Final 1.17 Upstream Update (Paper)
    PaperMC/Paper@4d6073f Partially fix #6880 (#6888)
    PaperMC/Paper@f8992ee Extend Entity by Sound.Emitter (#6890)
    PaperMC/Paper@db0bff9 Call onRemove logic for breakNaturally (#6854)
    PaperMC/Paper@72b731b Updated Upstream (CraftBukkit) (#6885)
    PaperMC/Paper@555f99d cleanup: Add hand to bucket events patch (#6881)
    PaperMC/Paper@51b7b60 Add API for resetting a single score (#6873)
    PaperMC/Paper@f3830bc Update paperweight to 1.2.0 (#6901)
    PaperMC/Paper@e22a1a9 Fix more namespacedkey parsing (#6903)
    PaperMC/Paper@284b3b8 Fix AnvilPrepareEvent not working with zero xp (#6895)
    PaperMC/Paper@22aaf91 Add config option to enable hopper occluding blocks (#6896)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,22 @@
+import io.papermc.paperweight.util.constants.*
+
 plugins {
     java
+    `maven-publish`
     id("com.github.johnrengelman.shadow") version "7.1.0" apply false
-    id("io.papermc.paperweight.patcher") version "1.1.14"
+    id("io.papermc.paperweight.patcher") version "1.2.0"
 }
 
 repositories {
     mavenCentral()
     maven("https://papermc.io/repo/repository/maven-public/") {
-        content {
-            onlyForConfigurations("paperclip")
-        }
+        content { onlyForConfigurations(PAPERCLIP_CONFIG) }
     }
 }
 
 dependencies {
     remapper("net.fabricmc:tiny-remapper:0.6.0:fat")
+    decompiler("net.minecraftforge:forgeflower:1.5.498.12")
     paperclip("io.papermc:paperclip:2.0.1")
 }
 
@@ -57,13 +59,13 @@ subprojects {
 paperweight {
     serverProject.set(project(":Purpur-Server"))
 
+    remapRepo.set("https://maven.fabricmc.net/")
+    decompileRepo.set("https://files.minecraftforge.net/maven/")
+
     usePaperUpstream(providers.gradleProperty("paperCommit")) {
         withPaperPatcher {
             apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
             apiOutputDir.set(layout.projectDirectory.dir("Purpur-API"))
-
-            remapRepo.set("https://maven.fabricmc.net/")
-            decompileRepo.set("https://files.minecraftforge.net/maven/")
 
             serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
             serverOutputDir.set(layout.projectDirectory.dir("Purpur-Server"))
