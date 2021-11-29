f0e15c4d2ab5cff34b47c7c59104c2f13f6c7a9f
Ben Kerllenevich <ben@omega24.dev>
2021-11-29 08:28:20 -0500

    rebase

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import io.papermc.paperweight.util.constants.*
 plugins {
     java
     `maven-publish`
-    id("com.github.johnrengelman.shadow") version "7.1.0" apply false
+    id("com.github.johnrengelman.shadow") version "7.1.0"
     id("io.papermc.paperweight.patcher") version "1.3.0-SNAPSHOT"
 }
 
@@ -17,7 +17,7 @@ repositories {
 dependencies {
     remapper("net.fabricmc:tiny-remapper:0.7.0:fat")
     decompiler("net.minecraftforge:forgeflower:1.5.498.22")
-    paperclip("io.papermc:paperclip:2.0.1")
+    paperclip("io.papermc:paperclip:3.0.2-SNAPSHOT")
 }
 
 subprojects {
@@ -25,8 +25,6 @@ subprojects {
     apply(plugin = "maven-publish")
 
     java {
-        sourceCompatibility = JavaVersion.VERSION_16
-        targetCompatibility = JavaVersion.VERSION_16
         toolchain {
             languageVersion.set(JavaLanguageVersion.of(17))
         }
