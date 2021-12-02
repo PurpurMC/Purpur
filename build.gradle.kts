e5932647158149148552c5d60474246f395c8a54
Ben Kerllenevich <ben@omega24.dev>
2021-12-02 06:39:53 -0500

    upstream

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import io.papermc.paperweight.util.constants.*
 plugins {
     java
     `maven-publish`
-    id("com.github.johnrengelman.shadow") version "7.1.0"
-    id("io.papermc.paperweight.patcher") version "1.3.0-SNAPSHOT"
+    id("com.github.johnrengelman.shadow") version "7.1.0" apply false
+    id("io.papermc.paperweight.patcher") version "1.3.1"
 }
 
 repositories {
