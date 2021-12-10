26e5f080f23b11dddd70dfcb10f39d81c392458f
Encode42 <me@encode42.dev>
2021-12-09 22:46:12 -0500

    Updated Upstream (Paper)
    Upstream has released updates that appear to apply and compile correctly
    
    Paper Changes:
    PaperMC/Paper@b4192fd fix NPE from changes in e4358b82171
    PaperMC/Paper@5b6445a Revert "fix NPE from changes in e4358b82171"
    PaperMC/Paper@323c087 Revert "#686: Fix contains for default section generating real sections"
    PaperMC/Paper@c837002 Fix client world difficulty sync issue (#7035)
    PaperMC/Paper@a4782f7 [ci skip] fixup indent
    PaperMC/Paper@83aee0f [ci skip] Clarify setSize consequences for Slimes (#7036)
    PaperMC/Paper@7c8fdc1 Add dropped hunk from mid-tick tasks (#7034)
    PaperMC/Paper@fd263ef Fix empty/null chunk section check in LevelChunk#getBlockData, rename… (#7039)
    PaperMC/Paper@b8d486c Create workflow to add new PRs to the PR Queue project (#6918)
    PaperMC/Paper@a50e273 Include axolotls in affected entities for water splash potions (#7024)
    PaperMC/Paper@af95df8 Port Actually unload POI data from Tuinity 1.16 (#7044)
    PaperMC/Paper@04897b1 [ci skip] Revert "Create workflow to add new PRs to the PR Queue project (#6918)" (#7046)
    PaperMC/Paper@b4a77a8 Updated Upstream (Bukkit/CraftBukkit) (#7045)
    PaperMC/Paper@0e25db2 Fix mis-placed processEnchantOrder from 1.18 update (#7052)
    PaperMC/Paper@53d026e Fix unused EntitySectionStorage#getEntities(AABB, Consumer) method being broken
    PaperMC/Paper@772e880 Fix light propagation in high y sections
    PaperMC/Paper@33ea869 Bump Starlight light version
    PaperMC/Paper@74fd151 Fix entity equipment on cancellation of EntityDeathEvent (#5740)
    PaperMC/Paper@758e2a7 Fix bad ticking checks for blocks
    PaperMC/Paper@0e91b6a Return 0 for light values if a dimenion does not have them
    PaperMC/Paper@188a8df Fix ChunkSnapshot#isSectionEmpty(int)
    PaperMC/Paper@bbc7451 Fix issue with snapshotted biomes in last commit
    PaperMC/Paper@b475c6a Backport log4j fix
    PaperMC/Paper@4e355c4 Updated Upstream (CraftBukkit)
    PaperMC/Paper@dce79f3 Update Log4J (#7069)

---
+++ scripts/upstreamCommit.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+# requires curl & jq
+
+# upstreamCommit <baseHash>
+# param: bashHash - the commit hash to use for comparing commits (baseHash...HEAD)
+
+(
+set -e
+PS1="$"
+
+paper=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/PaperMC/Paper/compare/$1...HEAD | jq -r '.commits[] | "PaperMC/Paper@\(.sha[:7]) \(.commit.message | split("\r\n")[0] | split("\n")[0])"')
+
+updated=""
+logsuffix=""
+if [ ! -z "paper" ]; then
+    logsuffix="$logsuffix\n\nPaper Changes:\n$paper"
+    updated="Paper"
+fi
+disclaimer="Upstream has released updates that appear to apply and compile correctly"
+
+log="${UP_LOG_PREFIX}Updated Upstream ($updated)\n\n${disclaimer}${logsuffix}"
+
+echo -e "$log" | git commit -F -
+
+) || exit 1
