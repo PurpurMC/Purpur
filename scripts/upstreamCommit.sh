4ff0630086bb589e1e421b06333eeeb4a3a027b2
Encode42 <me@encode42.dev>
2021-12-10 00:17:18 -0500

    Updated Upstream (Paper)
    Upstream has released updates that appear to apply and compile correctly
    
    Paper Changes:

--- scripts/upstreamCommit.sh
+++ scripts/upstreamCommit.sh
@@ -1,25 +1,23 @@
 #!/usr/bin/env bash
+
+# requires curl & jq
+
+# upstreamCommit <oldHash> <newHash>
+# param: bashHash - the commit hash to use for comparing commits (baseHash...HEAD)
+
 (
 set -e
 PS1="$"
 
-function changelog() {
-    base=$(git ls-tree HEAD $1  | cut -d' ' -f3 | cut -f1)
-    cd $1 && git log --oneline ${base}...HEAD
-}
-paper=$(changelog .gradle/caches/paperweight/upstreams/paper)
+paper=$(curl -H "Accept: application/vnd.github.v3+json" https://api.github.com/repos/PaperMC/Paper/compare/$1...$2 | jq -r '.commits[] | "PaperMC/Paper@\(.sha[:7]) \(.commit.message | split("\r\n")[0] | split("\n")[0])"')
 
 updated=""
 logsuffix=""
-if [ ! -z "$paper" ]; then
+if [ ! -z "paper" ]; then
     logsuffix="$logsuffix\n\nPaper Changes:\n$paper"
     updated="Paper"
 fi
 disclaimer="Upstream has released updates that appear to apply and compile correctly"
-
-if [ ! -z "$1" ]; then
-    disclaimer="$@"
-fi
 
 log="${UP_LOG_PREFIX}Updated Upstream ($updated)\n\n${disclaimer}${logsuffix}"
 
