398d5b9243099abc46fbe9ea597ba83ef0f4b41f
BillyGalbreath <blake.galbreath@gmail.com>
2022-06-12 16:38:55 -0500

    Updated Upstream (Paper)
    Upstream has released updates that appear to apply and compile correctly
    
    Paper Changes:
    PaperMC/Paper@4797e3a Dont reassign the hiddenConfigs field in TimingsManager (#7951)
    PaperMC/Paper@3690440 Update paperweight to 1.3.7
    PaperMC/Paper@90050ff Update Adventure to 4.11.0 and implement ComponentLogger (#7937)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     java
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "7.1.2" apply false
-    id("io.papermc.paperweight.patcher") version "1.3.6"
+    id("io.papermc.paperweight.patcher") version "1.3.7"
 }
 
 repositories {
