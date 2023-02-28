c5f1057812dee70c437e4f124bc7b29ad3807095
granny <granny@purpurmc.org>
2023-02-28 05:54:09 -0800

    Updated Upstream (Paper)
    Upstream has released updates that appear to apply and compile correctly
    
    Paper Changes:
    PaperMC/Paper@83a1304 [ci skip] Update Gradle wrapper to 8.0.1 (#8924)
    PaperMC/Paper@08e6622 fix runServer tasks
    PaperMC/Paper@4ae202e Fix getBlockStateIfLoaded world bounds check
    PaperMC/Paper@b670798 Close plugin jar file on classloader close and after retrieving name for updating (#8902)
    PaperMC/Paper@5170d6f Update the flag when a captured block state is outdated (#8860)
    PaperMC/Paper@dead691 Fix MC-157464 Prevent sleeping villagers moving towards food (#8923)
    PaperMC/Paper@f408c25 Log message if plugin bootstrap provided external plugin instead of throwing an exception (#8914)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     java
     `maven-publish`
-    id("com.github.johnrengelman.shadow") version "7.1.2" apply false
-    id("io.papermc.paperweight.patcher") version "1.5.1"
+    id("com.github.johnrengelman.shadow") version "8.1.0" apply false
+    id("io.papermc.paperweight.patcher") version "1.5.2"
 }
 
 allprojects {
