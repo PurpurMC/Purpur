ea2835bfe1e1625a02f7c7d26c06fe34a6674be1
granny <granny@purpurmc.org>
2023-11-09 09:59:08 -0800

    Updated Upstream (Paper)
    Upstream has released updates that appear to apply and compile correctly
    
    Paper Changes:
    PaperMC/Paper@274e54b Bump tiny-remapper for Java 21 support (#9902)
    PaperMC/Paper@4675152 Don't leave the NearbyPlayers tracker in an entirely busted state on double-add detection

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,7 +57,7 @@ repositories {
 }
 
 dependencies {
-    remapper("net.fabricmc:tiny-remapper:0.8.6:fat")
+    remapper("net.fabricmc:tiny-remapper:0.8.10:fat")
     decompiler("net.minecraftforge:forgeflower:2.0.627.2")
     paperclip("io.papermc:paperclip:3.0.3")
 }
