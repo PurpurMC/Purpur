37b9ad3bd71fd6b58f98c91d63c6d98e4c26e3d9
granny <granny@purpurmc.org>
2024-04-30 19:20:51 -0700

    Updated Upstream (Paper)
    Upstream has released updates that appear to apply and compile correctly
    
    Paper Changes:
    PaperMC/Paper@4ea67ab [ci skip] Remove trailing comma in projects action
    PaperMC/Paper@8c058f8 Update paperweight to 1.6.3 (#10626)
    PaperMC/Paper@fa6013d Fixes beds not exploding in the nether
    PaperMC/Paper@3e0eb4a Updated Upstream (CraftBukkit) (#10624)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 plugins {
     java
     `maven-publish`
-    id("io.papermc.paperweight.patcher") version "1.6.2"
+    id("io.papermc.paperweight.patcher") version "1.6.3"
 }
 
 allprojects {
