087d43d00bb1275645b578f27472b3c07c1c1a50
granny <granny@purpurmc.org>
2024-05-02 14:50:02 -0700

    [ci skip] add test plugin module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -37,3 +37,21 @@ for (name in listOf("Purpur-API", "Purpur-Server", "paper-api-generator")) {
     include(projName)
     findProject(":$projName")!!.projectDir = file(name)
 }
+
+optionalInclude("test-plugin")
+
+fun optionalInclude(name: String, op: (ProjectDescriptor.() -> Unit)? = null) {
+    val settingsFile = file("$name.settings.gradle.kts")
+    if (settingsFile.exists()) {
+        apply(from = settingsFile)
+        findProject(":$name")?.let { op?.invoke(it) }
+    } else {
+        settingsFile.writeText(
+            """
+            // Uncomment to enable the '$name' project
+            // include(":$name")
+
+            """.trimIndent()
+        )
+    }
+}
---
+++ test-plugin/src/main/java/org/purpurmc/testplugin/TestPluginBootstrap.java
@@ -0,0 +1,4 @@
+package org.purpurmc.testplugin;
+
+public class TestPluginBootstrap {
+}
