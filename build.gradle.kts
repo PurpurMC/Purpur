2a50638047508494d4289ca66fbbc094a388a710
granny <granny@purpurmc.org>
2024-09-29 00:15:17 -0700

    Updated Upstream (Paper)
    Upstream has released updates that appear to apply and compile correctly
    
    Paper Changes:
    PaperMC/Paper@41bc31b Update paperweight to 1.7.3 (#11445)
    PaperMC/Paper@e17eb6b Improve entity effect API (#11444)
    PaperMC/Paper@7b03141 Add startingBrewTime (#11406)
    PaperMC/Paper@355b1cb Add API for explosions to damage the explosion cause (#11180)
    PaperMC/Paper@6d7a438 Call bucket events for cauldrons (#7486)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 plugins {
     java
     `maven-publish`
-    id("io.papermc.paperweight.patcher") version "1.7.1"
+    id("io.papermc.paperweight.patcher") version "1.7.3"
 }
 
 allprojects {
