c16382875c0823e2bfc76c4e48b73aea0d1bac4e
William Blake Galbreath <blake.galbreath@gmail.com>
2025-01-09 15:03:25 -0800

    Stop squids floating on top of water

--- purpur-server/src/main/java/org/purpurmc/purpur/PurpurWorldConfig.java
+++ purpur-server/src/main/java/org/purpurmc/purpur/PurpurWorldConfig.java
@@ -1600,6 +1600,7 @@ public class PurpurWorldConfig {
     public double squidMaxHealth = 10.0D;
     public double squidScale = 1.0D;
     public boolean squidImmuneToEAR = true;
+    public double squidOffsetWaterCheck = 0.0D;
     private void squidSettings() {
         squidRidable = getBoolean("mobs.squid.ridable", squidRidable);
         squidControllable = getBoolean("mobs.squid.controllable", squidControllable);
@@ -1611,6 +1612,7 @@ public class PurpurWorldConfig {
         squidMaxHealth = getDouble("mobs.squid.attributes.max_health", squidMaxHealth);
         squidScale = Mth.clamp(getDouble("mobs.squid.attributes.scale", squidScale), 0.0625D, 16.0D);
         squidImmuneToEAR = getBoolean("mobs.squid.immune-to-EAR", squidImmuneToEAR);
+        squidOffsetWaterCheck = getDouble("mobs.squid.water-offset-check", squidOffsetWaterCheck);
     }
 
     public boolean spiderRidable = false;
