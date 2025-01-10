218e3491c187ef0ac95d6c917af1d396813d6bd5
William Blake Galbreath <blake.galbreath@gmail.com>
2025-01-10 12:39:08 -0800

    Fix stuck in portals

--- purpur-server/src/main/java/org/purpurmc/purpur/PurpurWorldConfig.java
+++ purpur-server/src/main/java/org/purpurmc/purpur/PurpurWorldConfig.java
@@ -319,6 +319,7 @@ public class PurpurWorldConfig {
     public int playerDeathExpDropMax = 100;
     public boolean teleportIfOutsideBorder = false;
     public boolean totemOfUndyingWorksInInventory = false;
+    public boolean playerFixStuckPortal = false;
     private void playerSettings() {
         if (PurpurConfig.version < 19) {
             boolean oldVal = getBoolean("gameplay-mechanics.player.idle-timeout.mods-target", idleTimeoutTargetPlayer);
@@ -334,6 +335,7 @@ public class PurpurWorldConfig {
         playerDeathExpDropMax = getInt("gameplay-mechanics.player.exp-dropped-on-death.maximum", playerDeathExpDropMax);
         teleportIfOutsideBorder = getBoolean("gameplay-mechanics.player.teleport-if-outside-border", teleportIfOutsideBorder);
         totemOfUndyingWorksInInventory = getBoolean("gameplay-mechanics.player.totem-of-undying-works-in-inventory", totemOfUndyingWorksInInventory);
+        playerFixStuckPortal = getBoolean("gameplay-mechanics.player.fix-stuck-in-portal", playerFixStuckPortal);
     }
 
     public boolean silkTouchEnabled = false;
