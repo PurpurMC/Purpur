a9fe8f35d0360da5b11105d344744d1d30cbc14d
William Blake Galbreath <blake.galbreath@gmail.com>
2025-01-12 12:43:19 -0800

    Shift right click to use exp for mending

--- purpur-server/src/main/java/org/purpurmc/purpur/PurpurWorldConfig.java
+++ purpur-server/src/main/java/org/purpurmc/purpur/PurpurWorldConfig.java
@@ -405,6 +405,7 @@ public class PurpurWorldConfig {
     public boolean playerBurpWhenFull = false;
     public boolean playerRidableInWater = false;
     public boolean playerRemoveBindingWithWeakness = false;
+    public int shiftRightClickRepairsMendingPoints = 0;
     private void playerSettings() {
         if (PurpurConfig.version < 19) {
             boolean oldVal = getBoolean("gameplay-mechanics.player.idle-timeout.mods-target", idleTimeoutTargetPlayer);
@@ -429,6 +430,7 @@ public class PurpurWorldConfig {
         playerBurpWhenFull = getBoolean("gameplay-mechanics.player.burp-when-full", playerBurpWhenFull);
         playerRidableInWater = getBoolean("gameplay-mechanics.player.ridable-in-water", playerRidableInWater);
         playerRemoveBindingWithWeakness = getBoolean("gameplay-mechanics.player.curse-of-binding.remove-with-weakness", playerRemoveBindingWithWeakness);
+        shiftRightClickRepairsMendingPoints = getInt("gameplay-mechanics.player.shift-right-click-repairs-mending-points", shiftRightClickRepairsMendingPoints);
     }
 
     public boolean silkTouchEnabled = false;
