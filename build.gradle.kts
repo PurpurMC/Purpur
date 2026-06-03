9016317875bf5b0a763774a2a74d1540bef08e89
Clos-Rise <chernaev.alex@mail.ru>
2026-06-03 16:09:26 +0500

    Initial Hikari commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,90 +1,36 @@
-import org.gradle.api.tasks.testing.logging.TestExceptionFormat
-import org.gradle.api.tasks.testing.logging.TestLogEvent
-
 plugins {
-    java // TODO java launcher tasks
     id("io.papermc.paperweight.patcher") version "2.0.0-SNAPSHOT"
 }
 
-val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    remapper("net.fabricmc:tiny-remapper:0.10.4:fat")
+    decompiler("org.vineflower:vineflower:1.10.1")
+    paperclip("io.papermc:paperclip:3.0.3")
+}
 
 paperweight {
-    upstreams.paper {
-        ref = providers.gradleProperty("paperCommit")
+    serverProject.set(project(":hikari-server"))
 
-        patchFile {
-            path = "paper-server/build.gradle.kts"
-            outputFile = file("purpur-server/build.gradle.kts")
-            patchFile = file("purpur-server/build.gradle.kts.patch")
-        }
-        patchFile {
-            path = "paper-api/build.gradle.kts"
-            outputFile = file("purpur-api/build.gradle.kts")
-            patchFile = file("purpur-api/build.gradle.kts.patch")
-        }
-        patchDir("paperApi") {
-            upstreamPath = "paper-api"
-            excludes = setOf("build.gradle.kts")
-            patchesDir = file("purpur-api/paper-patches")
-            outputDir = file("paper-api")
+    remapRepo.set("https://repo.maven.apache.org/maven2/")
+    decompileRepo.set("https://repo.maven.apache.org/maven2/")
+
+    usePaperUpstream(providers.gradleProperty("paperRef")) {
+        withPaperPatcher {
+            apiPatchDir.set(layout.projectDirectory.dir("patches/api"))
+            serverPatchDir.set(layout.projectDirectory.dir("patches/server"))
         }
     }
 }
 
-subprojects {
-    apply(plugin = "java-library")
-    apply(plugin = "maven-publish")
+allprojects {
+    apply(plugin = "java")
 
-    extensions.configure<JavaPluginExtension> {
-        toolchain {
-            languageVersion = JavaLanguageVersion.of(25)
-        }
-    }
-
-    tasks.withType<JavaCompile> {
-        options.encoding = Charsets.UTF_8.name()
-        options.release = 25
-        options.isFork = true
-        options.compilerArgs.addAll(listOf("-Xlint:-deprecation", "-Xlint:-removal"))
-    }
-    tasks.withType<Javadoc> {
-        options.encoding = Charsets.UTF_8.name()
-    }
-    tasks.withType<ProcessResources> {
-        filteringCharset = Charsets.UTF_8.name()
-    }
-    tasks.withType<Test> {
-        testLogging {
-            showStackTraces = true
-            exceptionFormat = TestExceptionFormat.FULL
-            events(TestLogEvent.STANDARD_OUT)
-        }
-    }
-
-    repositories {
-        mavenCentral()
-        maven(paperMavenPublicUrl)
-        maven("https://jitpack.io")
-    }
-
-    extensions.configure<PublishingExtension> {
-        repositories {
-            maven("https://repo.purpurmc.org/snapshots") {
-                name = "purpur"
-                credentials(PasswordCredentials::class)
-            }
-        }
-    }
-}
-
-tasks.register("printMinecraftVersion") {
-    doLast {
-        println(providers.gradleProperty("mcVersion").get().trim())
-    }
-}
-
-tasks.register("printPurpurVersion") {
-    doLast {
-        println(project.version)
+    java {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(21))
     }
 }
