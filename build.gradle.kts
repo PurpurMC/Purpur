a6505f4b6eee3cb4df19e53f41115b898e9521e1
granny <granny@purpurmc.org>
2026-08-27 18:36:19 -0700

    [ci skip] mirror paper meta project changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import io.papermc.paperweight.checkstyle.PaperCheckstyleExt
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
@@ -48,12 +49,37 @@ paperweight {
 }
 
 subprojects {
-    apply(plugin = "java-library")
-    apply(plugin = "maven-publish")
+    apply {
+        plugin("java-library")
+        plugin("maven-publish")
+    }
 
     extensions.configure<JavaPluginExtension> {
         toolchain {
             languageVersion = JavaLanguageVersion.of(25)
+        }
+    }
+
+    val tempDisabled = setOf("purpur-server", "paper-server", "test-plugin")
+
+    if (name !in tempDisabled) {
+        apply { plugin("io.papermc.paperweight.paper-checkstyle") }
+        extensions.configure<PaperCheckstyleExt> {
+            typeUseAnnotationsFile.set(rootProject.layout.projectDirectory.file(".checkstyle/type_use_annotations.txt"))
+        }
+
+        /*tasks.withType<PaperCheckstyleTask>().configureEach {
+            configDirectory = rootProject.layout.projectDirectory.dir(".checkstyle")
+            // configFile = layout.projectDirectory.file(".checkstyle/checkstyle.xml").asFile // use the base file if not overwritten
+            maxHeapSize = "2g"
+            reports {
+                xml.required = true
+                html.required = true
+            }
+        }*/
+
+        dependencies {
+            "checkstyle"(project(":purpur-checkstyle"))
         }
     }
 
